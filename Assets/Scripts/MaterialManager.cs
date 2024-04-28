b33c4de37e1a0de24f8a082a9f2c3dc41f3e6f54
XiRdnethFr <yg2540@nyu.edu>
2024-04-29 03:02:44 +0800

    fixed stuff

--- Assets/Scripts/MaterialManager.cs
+++ Assets/Scripts/MaterialManager.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class MaterialManager : MonoBehaviour
 {
-    public Material normalMaterial;  // 普通状态的材质
-    public Material highlightMaterial;  // 高亮状态的材质
+    public Material normalMaterial;
+    public Material highlightMaterial;
 
     public bool isKey;
 
@@ -14,7 +14,7 @@ public class MaterialManager : MonoBehaviour
     private void Awake()
     {
         objRenderer = GetComponent<Renderer>();
-        objRenderer.material = normalMaterial;  // 初始时使用普通材质
+        objRenderer.material = normalMaterial;
         trigger.SetActive(false);
     }
 
@@ -22,7 +22,7 @@ public class MaterialManager : MonoBehaviour
     {
         if (highlightMaterial != null)
         {
-            objRenderer.material = highlightMaterial;  // 应用高亮材质
+            objRenderer.material = highlightMaterial;
         }
     }
 
@@ -30,7 +30,7 @@ public class MaterialManager : MonoBehaviour
     {
         if (normalMaterial != null)
         {
-            objRenderer.material = normalMaterial;  // 恢复普通材质
+            objRenderer.material = normalMaterial;
         }
     }
 
